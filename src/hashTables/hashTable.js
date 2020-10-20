56f938bde74702f5a336819a8f6225cd0faee974
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-10-20 20:52:47 -0300

    Create hash table implementation

---
+++ src/hashTables/hashTable.js
@@ -0,0 +1,47 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+
+    this.data[address].push([key, value]);
+
+    return this.data;
+  }
+
+  get(key) {
+    const address = this._hash(key);
+    const currentBucket = this.data[address];
+
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+
+    return undefined;
+  }
+
+  _hash(key) {
+    let hash = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+
+    return hash;
+  }
+}
+
+const myHashTable = new HashTable(50);
+
+myHashTable.set('grapes', 5);
+console.log(myHashTable.get('grapes'));
