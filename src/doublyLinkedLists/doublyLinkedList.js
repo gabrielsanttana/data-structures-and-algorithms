13074d6c1edfe4e8c233874b7d4345afc6b7f806
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-11-04 20:18:21 -0300

    Add doubly linked list prepend, insert and remove methods

--- src/doublyLinkedLists/doublyLinkedList.js
+++ src/doublyLinkedLists/doublyLinkedList.js
@@ -2,7 +2,7 @@ class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
-    this.before = null;
+    this.previous = null;
   }
 }
 
@@ -11,7 +11,7 @@ class DoublyLinkedList {
     this.head = {
       value: value,
       next: null,
-      before: null,
+      previous: null,
     };
     this.tail = this.head;
     this.length = 1;
@@ -20,7 +20,7 @@ class DoublyLinkedList {
   append(value) {
     const newNode = new Node(value);
 
-    this.tail.before = this.traverseToIndex();
+    newNode.previous = this.tail;
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
@@ -28,11 +28,46 @@ class DoublyLinkedList {
     return this;
   }
 
-  prepend() {}
+  prepend(value) {
+    const newNode = new Node(value);
 
-  insert() {}
+    newNode.next = this.head;
+    this.head.previous = newNode;
+    this.head = newNode;
+    this.length++;
 
-  remove(index) {}
+    return this;
+  }
+
+  insert(index, value) {
+    const newNode = new Node(value);
+
+    const leaderNode = this.traverseToIndex(index - 1);
+    const holdingNode = leaderNode.next;
+
+    leaderNode.next = newNode;
+    newNode.next = holdingNode;
+    newNode.previous = leaderNode;
+    holdingNode.previous = newNode;
+
+    this.length++;
+
+    return this;
+  }
+
+  remove(index) {
+    const nodeToRemove = this.traverseToIndex(index);
+
+    const nodeToRemovePrevious = nodeToRemove.previous;
+    const nodeToRemoveNext = nodeToRemove.next;
+
+    nodeToRemovePrevious.next = nodeToRemoveNext;
+    nodeToRemoveNext.previous = nodeToRemovePrevious;
+
+    this.length--;
+
+    return this;
+  }
 
   traverseToIndex(index) {
     let counter = 0;
@@ -58,3 +93,17 @@ class DoublyLinkedList {
     return linkedListNodes;
   }
 }
+
+const linkedList = new DoublyLinkedList(10);
+
+linkedList.append(11);
+linkedList.append(12);
+console.log(linkedList.print());
+linkedList.prepend(9);
+linkedList.prepend(8);
+console.log(linkedList.print());
+linkedList.insert(1, 55);
+linkedList.insert(3, 88);
+console.log(linkedList.print());
+linkedList.remove(2);
+console.log(linkedList.print());
